ee19f9418b0a0d202a3cd7d1274f5fa3cd220ab7
souradeep-das <dsouradeep2@gmail.com>
2020-08-24 22:58:14 +0530

    inspect

--- apps/itest/test/itest/standard_exit_test.exs
+++ apps/itest/test/itest/standard_exit_test.exs
@@ -40,6 +40,7 @@ defmodule StandardExitsTests do
     # We need both initial_eth_balance and initial_balance because in ERC-20 case we check both
     initial_eth_balance = Itest.Poller.root_chain_get_balance(alice_account, Currency.ether())
     IO.inspect(initial_eth_balance)
+    IO.inspect(currency)
     initial_balance = Itest.Poller.root_chain_get_balance(alice_account, currency)
     IO.inspect(currency)
     IO.inspect(initial_balance)
